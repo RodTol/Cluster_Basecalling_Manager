3844ab743057b93d9fb05cac7b9e1d9448b032dc
RodTol <rodolfo.tolloi@gmail.com>
2023-11-06 13:44:34 +0000

    pod5

--- dataset/README.md
+++ dataset/README.md
@@ -1,3 +1,4 @@
 # Notes on the dataset 
 Most of the test for these infastructure where made using the jain/Homo_sapiens-FAF09968/fast5/Bham/FAF09968-3439856925_Multi dataset from the nanopore_benchmark repo.
-In order to expand this dataset, I used a simple py script that creates new random id but use the same files
+In order to expand this dataset, I used a simple py script that creates new random id but use the same files  
+To convert fast5 files to pod5 format I used the python API as described in this guide: [link](https://pod5-file-format.readthedocs.io/en/latest/docs/tools.html#pod5-view) 
--- utility/execute_run_local_dataset.sh
+++ utility/execute_run_local_dataset.sh
@@ -4,7 +4,6 @@ dcgmi stats -g 4 -a -v
 
 dcgmi stats -g 4 -e
 echo "What is the name of this run?"
-
 read run_name
 
 mkdir -p  /raid/tolloi-tests/results/guppy-results/$run_name
@@ -16,8 +15,8 @@ spack load ont-guppy@6.1.7-cuda
 nvidia-smi --query-gpu=timestamp,pci.bus_id,utilization.gpu,utilization.memory --format=csv -l 1 -f /raid/tolloi-tests/results/guppy-results/$run_name/gpu_log_$run_name.csv &
 bg_pid=$!
 
-#guppy_basecaller -c dna_r9.4.1_450bps_hac.cfg   -i  /raid/tolloi-tests/dataset_fast5_31G_0/ -s results/guppy-results/$run_name  -x cuda:all
-guppy_basecaller_supervisor --num_clients 25 --input_path $1 --save_path /raid/tolloi-tests/results/guppy-results/$run_name --config dna_r9.4.1_450bps_hac.cfg --port 42837 
+guppy_basecaller -c dna_r9.4.1_450bps_hac.cfg   -i  /raid/tolloi-tests/dataset_fast5_304G_0/ -s results/guppy-results/$run_name  -x cuda:all
+#guppy_basecaller_supervisor --num_clients 25 --input_path $1 --save_path /raid/tolloi-tests/results/guppy-results/$run_name --config dna_r9.4.1_450bps_hac.cfg --port 42837 
 
 kill $bg_pid
 
